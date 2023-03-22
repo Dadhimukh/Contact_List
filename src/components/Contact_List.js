308d17457fa0156439ea37e2604777200709f96c
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-22 10:20:54 +0530

    created two input one for name and another for number

--- src/components/Contact_List.js
+++ src/components/Contact_List.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Contact_List = () => {
   return (
-    <div>Welcome to Create Contact App</div>
-  )
-}
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="Enter the Name" />
+        </div>
+        <div>
+          <input type="number" placeholder="Enter the number" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Contact_List
+export default Contact_List;
