86dcc96acce8b1fc9e7dc9717e82ca10ccdb9e73
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:22:51 +0530

    Navbar section compleated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand ml-5">
+          Contact List App
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
