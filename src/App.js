d35378f88ebac828de0de721e731f9dd0340b90e
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:16:47 +0530

    used routes and created path for home and Add contact and edit contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route,Routes } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+
+
 function App() {
   return (
-    <>
-      <Contact_List />
-    </>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/add" element={<AddContact />}></Route>
+        <Route path="/edit/:id" element={<EditContact />}></Route>
+      </Routes>
+    </div>
   );
 }
 
