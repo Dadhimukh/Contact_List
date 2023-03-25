88336948d2ce948c6989f1e87510479f2a219d46
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:32:14 +0530

    AddContact btn and wher the contact is displayed it's alsao added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 my-5 text-end">
+          <Link to="/add" className="btn btn-outline-dark">
+            Add Contact
+          </Link>
+        </div>
+        <div className="col-md-10 mx-auto">
+          <table className="table table-hover">
+            <thead className="text-white bg-dark text-center">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Number</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
