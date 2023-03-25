5c174dd3ff6ed1a32e44a6602931708d9ccd5728
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:59:15 +0530

    Created store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+
+import App from './App';
+import contactReducer from './reducers/ContactReducer';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const store = createStore(contactReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
