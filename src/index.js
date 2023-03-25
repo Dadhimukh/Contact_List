a3050f4346f37ba5816be633c64b6a54af7972dd
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:18:50 +0530

    importing bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
