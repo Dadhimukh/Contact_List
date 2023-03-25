d7f9eac853f0854e2a9506c6ec6ee0d178b55cc0
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-25 14:42:57 +0530

    by using usestate changing the name and number and email onChange() and onSubmit handle the submit

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const AddContact = () => {
-  return (
-    <div>AddContact</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
 
-export default AddContact
+  const handelSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
+  
+  return (
+    <div className="container">
+      <h1 className="display-3 text-center fw-bold">Add Contact</h1>
+      <div className="row">
+        <div className="col-md-6 shadow mx-auto p-5">
+          <form className="text-center" onSubmit={handelSubmit}>
+            <div className="form-group mb-3">
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="email"
+                placeholder="Email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="number"
+                placeholder="Phone Number"
+                className="form-control"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="submit"
+                value="Add Contact"
+                className="btn btn-block btn-dark"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddContact;
