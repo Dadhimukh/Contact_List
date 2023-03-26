49ecb5683d05f669085f05a9ebba917244674228
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-26 15:50:28 +0530

    Added style file to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-2">
+    <nav className="navbar navbar-expand-lg navbar-dark py-2">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand ml-5">
           <img src="https://cdn-icons-png.flaticon.com/32/9839/9839447.png" />{" "}
-          Contact List
+          ContactList
         </Link>
         <div className=" text-end">
-          <Link to="/add" className="btn btn-outline-light fw-bold bg-success">
-            Add Contact
+          <Link to="/add" className="btn btn-outline-light fw-bold">
+            <img src="https://cdn-icons-png.flaticon.com/32/3059/3059520.png" />
           </Link>
         </div>
       </div>
