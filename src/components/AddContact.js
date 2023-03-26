80a6eab162c3fbbe8e203b2585b46caa2fe46979
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-26 14:09:00 +0530

    importing toast from react-toastify for showing pop messages

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddContact = () => {
   const [name, setName] = useState("");
