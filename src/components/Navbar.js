3645fc7866048a0fdfa3844ed8bdbfd819435293
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-26 15:29:17 +0530

    added image for contact list in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-2">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand ml-5">
+          <img src="https://cdn-icons-png.flaticon.com/32/9839/9839447.png" />{" "}
           Contact List
         </Link>
         <div className=" text-end">
