3069f5736976789952df2c8f0fb1474c3978de09
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-26 14:34:23 +0530

    importing toastify CSS for displaying pop messages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-
-import App from './App';
-import contactReducer from './reducers/ContactReducer';
+import App from "./App";
+import contactReducer from "./reducers/ContactReducer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const store = createStore(contactReducer, composeWithDevTools());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
