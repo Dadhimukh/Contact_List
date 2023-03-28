e2799c820ed371e81b380ca86aaea13dc14c0b29
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-29 00:15:53 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { ToastContainer } from "react-toastify";
-
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import AddContact from "./components/AddContact";
-import EditContact from "./components/EditContact";
+import React from 'react'
+import ContactList from './components/ContactList'
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const data = [];
-    const promise = async () => {
-      await fetch("https://jsonplaceholder.typicode.com/users/")
-        .then((response) => response.json())
-        .then((json) => {
-          json.map((contact) => {
-            data.push({
-              id: contact.id,
-              name: contact.name,
-              number: contact.phone,
-              email: contact.email,
-            });
-          });
-        });
-      dispatch({ type: "FETCH_CONTACTS", payload: data });
-    };
-    promise();
-  }, []);
-
   return (
-    <div className="App">
-      <BrowserRouter>
-        <ToastContainer />
-        <Navbar />
-
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="/add" element={<AddContact />}></Route>
-          <Route path="/edit/:id" element={<EditContact />}></Route>{" "}
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <ContactList />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/ContactList.js
@@ -0,0 +1,51 @@
+   import React, { useEffect } from "react";
+   import { BrowserRouter, Route, Routes } from "react-router-dom";
+   import { useDispatch } from "react-redux";
+   import { ToastContainer } from "react-toastify";
+
+   import Navbar from "./Navbar";
+   import Home from "./Home";
+   import AddContact from "./AddContact";
+   import EditContact from "./EditContact";
+
+   const ContactList = () => {
+   const dispatch = useDispatch();
+   useEffect(() => {
+      const data = [];
+      const promise = async () => {
+         await fetch("https://jsonplaceholder.typicode.com/users/")
+         .then((response) => response.json())
+         .then((json) => {
+            json.map((contact) => {
+               data.push({
+               id: contact.id,
+               name: contact.name,
+               number: contact.phone,
+               email: contact.email,
+               });
+            });
+         });
+         dispatch({ type: "FETCH_CONTACTS", payload: data });
+      };
+      promise();
+   }, []);
+
+   return (
+      <div className="App">
+         <BrowserRouter>
+         <ToastContainer />
+         <Navbar />
+
+         <Routes>
+            <Route path="/Contact_List">
+               <Route index element={<Home />} />
+            </Route>
+            <Route path="/add" element={<AddContact />}></Route>
+            <Route path="/edit/:id" element={<EditContact />}></Route>{" "}
+         </Routes>
+         </BrowserRouter>
+      </div>
+   );
+   };
+
+   export default ContactList;
